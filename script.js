880eb262ff50b9048952ad0ade83d165372e4ea0
FebyMakram <varvoramakram@gmail.com>
2024-10-05 13:42:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function scrollDown(){
+    window.scrollTo({
+        top:document.body.scrollHeight,
+        behavior:'smooth',
+    })
+}
+
+// function scrollTop(){
+//     window.scrollTo({
+//         top:0,
+//         behavior:'smooth',
+//     })
+// }
